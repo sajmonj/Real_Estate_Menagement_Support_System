3cc7734b87932fb2394c073da59c6d65bdc8cacc
Konrad1203 <147108244+Konrad1203@users.noreply.github.com>
2023-12-30 12:05:50 +0100

    Prosty system logowania i techniczna strona główna
    Szkielet systemu. Prosta lista mieszkań i widok mieszkania.

---
+++ strona/src/LoginSystem/registration.js
@@ -0,0 +1,152 @@
+import React, {useRef, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {UserManager} from "./userManager";
+
+export function Registration(props) {
+    const { users, registerUser, initialState } = UserManager();
+    const userInfoRef = useRef(initialState);
+    const repeatedPasswordRef = useRef("");
+    const [firstnameError, setFirstnameError] = useState("")
+    const [lastnameError, setLastnameError] = useState("")
+    const [emailError, setEmailError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+    const [repeatedPasswordError, setRepeatedPasswordError] = useState("")
+
+    const navigate = useNavigate();
+
+    function onButtonClick() {
+        setFirstnameError("")
+        setLastnameError("")
+        setEmailError("")
+        setPasswordError("")
+        setRepeatedPasswordError("")
+
+        const userInfo = userInfoRef.current;
+        const repeatedPassword = repeatedPasswordRef.current;
+
+        if ("" === userInfo.firstname) {
+            setFirstnameError("Please enter your firstname")
+            return
+        }
+        if (!/^[a-zA-Z]+$/.test(userInfo.firstname)) {
+            setFirstnameError("Please enter your real firstname")
+            return
+        }
+        if ("" === userInfo.lastname) {
+            setLastnameError("Please enter your lastname")
+            return
+        }
+        if (!/^[a-zA-Z]+$/.test(userInfo.lastname)) {
+            setLastnameError("Please enter your real lastname")
+            return
+        }
+        if ("" === userInfo.email) {
+            setEmailError("Please enter your email")
+            return
+        }
+        if (!/^[\w-x.]+@([\w-]+\.)+[\w-]{2,4}$/.test(userInfo.email)) {
+            setEmailError("Please enter a valid email")
+            return
+        }
+        if ("" === userInfo.password) {
+            setPasswordError("Please enter a password")
+            return
+        }
+        if (userInfo.password.length < 7) {
+            setPasswordError("The password must be 8 characters or longer")
+            return
+        }
+        if ("" === repeatedPassword) {
+            setRepeatedPasswordError("Please repeat your password")
+            return
+        }
+        if (userInfo.password !== repeatedPassword) {
+            setRepeatedPasswordError("Repeated password id different than the original password")
+            return
+        }
+
+        const user = users.find((user) => user.email === userInfo.email);
+
+        if (user) {
+            setEmailError("User with this email already exists");
+        } else {
+            const newUser = registerUser(userInfo);
+            props.setLoggedIn(true)
+            props.setUserInfo(newUser)
+            navigate("/")
+        }
+    }
+
+    return (
+        <div className="centeredDiv">
+            <div className="mainContainer">
+                <span className="hyperlink smallerText" onClick={() => navigate("/")}>&lt; Go back to the main page</span>
+                <div className="titleContainer">
+                    <div>Registration</div>
+                </div>
+                <br />
+                <div className="inputContainer">
+
+                    <label htmlFor="firstname">Firstname</label><br/>
+                    <input
+                        id="firstname"
+                        autoComplete="on"
+                        placeholder="Enter your firstname"
+                        onChange={ev => userInfoRef.current.firstname = ev.target.value }
+                        className="inputBox" />
+                    <br/><span className="errorLabel">{firstnameError}</span><br/>
+
+                    <label htmlFor="lastname">Lastname</label><br/>
+                    <input
+                        id="lastname"
+                        autoComplete="on"
+                        placeholder="Enter your lastname"
+                        onChange={ev => userInfoRef.current.lastname = ev.target.value }
+                        className="inputBox" />
+                    <br/><span className="errorLabel">{lastnameError}</span><br/>
+
+                    <label htmlFor="email">Email</label><br/>
+                    <input
+                        id="email"
+                        autoComplete="email"
+                        placeholder="Enter your email here"
+                        onChange={ev => userInfoRef.current.email = ev.target.value }
+                        className="inputBox" />
+                    <br/><span className="errorLabel">{emailError}</span><br/>
+
+                    <label htmlFor="password">Password</label><br/>
+                    <input
+                        id="password"
+                        autoComplete="new-password"
+                        type={"password"}
+                        placeholder="Enter your password here"
+                        onChange={ev => userInfoRef.current.password = ev.target.value }
+                        className="inputBox" />
+                    <br/><span className="errorLabel">{passwordError}</span><br/>
+
+                    <label htmlFor="repeatedPassword">Repeat your password</label><br/>
+                    <input
+                        id="repeatedPassword"
+                        autoComplete="new-password"
+                        type={"password"}
+                        placeholder="Repeat your password"
+                        onChange={ev => repeatedPasswordRef.current = ev.target.value }
+                        className="inputBox" />
+                    <br/><span className="errorLabel">{repeatedPasswordError}</span><br/>
+
+                    <div className="centered">
+                        <input
+                        className="inputButton greenButton"
+                        type="button"
+                        onClick={onButtonClick}
+                        value={"Log in"} />
+                    </div>
+                </div>
+
+                <br/><br/><span>Already have an account?{' '} <span className="hyperlink hyperlinkBlue" onClick={() => navigate("/login")}>Log in</span> </span>
+
+            </div>
+        </div>
+
+    );
+}
---
+++ strona/src/apartmentSystem/apartmentView.js
@@ -0,0 +1,40 @@
+import {useNavigate, useParams} from "react-router-dom";
+import {ApartmentManager} from "./apartmentManager";
+import {useEffect} from "react";
+
+export function ApartmentView(props) {
+    const {id} = useParams();
+    const {loggedIn, userInfo} = props;
+    const navigate = useNavigate();
+    const {apartments, getDevelopmentTypeName} = ApartmentManager();
+
+    const apartment = apartments.find(apartment => apartment.id === parseInt(id));
+
+
+
+    useEffect(() => {
+        if (!loggedIn) navigate('/');
+    }, [loggedIn, navigate]);
+
+    return (
+        <>
+            {apartment ?
+                <>
+                    <span className="hyperlink smallerText" onClick={() => navigate("/")}>&lt; Go back to the main page</span>
+                    <h1>Apartment view: {id}</h1>
+                    <div>Adres: {apartment.city} {apartment.street} {apartment.buildingNumber}/{apartment.apartmentNumber}</div>
+                    <div>Adres: {apartment.country} {apartment.postalCode}</div>
+                    <div>Typ zabudowy: {getDevelopmentTypeName(apartment.developmentType)}</div>
+                    <div>Powierzchnia: {apartment.area} m2</div>
+                    <div>Stan: {apartment.furnished ? "Umeblowany" : "Nie umeblowany"}</div>
+                    <div>Piętro: {apartment.floor}</div>
+                    <div>Cena: {apartment.price} PLN</div>
+                    <div>Liczba pokoi: {apartment.rooms}</div>
+                    <div>Opis: {apartment.description}</div>
+                    <img src={apartment.pictures[0]} alt="Apartment picture"/>
+                </>
+                : <div>Apartment not found</div>
+            }
+        </>
+    )
+}

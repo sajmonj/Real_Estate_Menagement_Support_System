9df79dcdda9c97925f405658de9a65eb9b87f9d8
Konrad1203 <147108244+Konrad1203@users.noreply.github.com>
2024-01-20 15:05:23 +0100

    Stopping button propagation

--- strona/src/apartmentSystem/apartmentOnList.js
+++ strona/src/apartmentSystem/apartmentOnList.js
@@ -6,21 +6,13 @@ export function ApartmentOnList({apartment, navigate, openPopup}) {
 
     return (
         <div className="apartmentOnListView" onClick={() => navigate(`/apartments/${apartment.id}`)}>
-            {/*<img className="apartmentSmallPicture" src={apartment.photos != null ? apartment.photos[0] : 'apartmentPlaceholder.png'} alt={apartment.title} />*/}
             <div>
                 <h3>{apartment.street} {apartment.streetNumber}</h3>
-                {/*<p className="closeText">{apartment.description}</p><br/>*/}
                 <div className="infoGrid">
                     <div className="closeText">
                         <p>{apartment.street.length === 0 ? apartment.city : "ul. " + apartment.street} {apartment.streetNumber}{apartment.apartmentNumber === 0 ? "" : "/" + apartment.apartmentNumber}</p>
                         <p>{apartment.zipCode} {apartment.city}</p>
-                        {/*<p>{apartment.country}</p>*/}
                     </div>
-                    {/*<div className='closeText'>*/}
-                    {/*    <p>Typ: {getDevelopmentTypeName(apartment.developmentType)}</p>*/}
-                    {/*    <p>Stan: {apartment.furnished ? "Umeblowany" : "Nie umeblowany"}</p>*/}
-                    {/*    <p>Piętro: {apartment.floor}</p>*/}
-                    {/*</div>*/}
                     <div className='closeText'>
                         <p>Rooms: {apartment.rooms}</p>
                         <p>Kitchens: {apartment.kitchens}</p>
@@ -34,10 +26,11 @@ export function ApartmentOnList({apartment, navigate, openPopup}) {
                     </div>
                 </div>
             </div>
-            <div className="buttonPanel">
-                <Button variant="outline-primary" style={{margin: 5}} onClick={() => navigate(`/edit-apartment/${apartment.id}`)}>Edit</Button>
-                <Button variant="outline-danger" style={{margin: 5}} onClick={() => openPopup(apartment.id)}>Remove</Button>
-            </div>
+            <Button variant="outline-primary" style={{margin: 5}} onClick={(e) => {
+                    navigate(`/edit-apartment/${apartment.id}`);
+                    e.stopPropagation();}}>Edit</Button>
+                <Button variant="outline-danger" style={{margin: 5}} onClick={(e) => {
+                    openPopup(apartment.id); e.stopPropagation(); }}>Remove</Button>
         </div>
     );
 }
